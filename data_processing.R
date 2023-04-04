3a5021cf590a4ea56ccab1ed4747cc47339004f3
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-04-04 14:12:38 +0200

    Update data_processing.R

--- data_processing.R
+++ data_processing.R
@@ -12,11 +12,16 @@ data <- read.csv(datafile,sep=";")
 dates_char <- as.character(data$dates)
 dates_char[1] #
 tail(dates_char,1) #
-dates <- as.yearmon(seq(from=1990, to=2023, by=1/12))
+dates <- as.yearmon(seq(from=2023, to=1990, by=-1/12))
 
 prod <- zoo(data$values, order.by=dates)
 
 plot(prod)
+
+diff_prod = diff(prod,1)
+
+plot(diff_prod)
+
 
 
 
