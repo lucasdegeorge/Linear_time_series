3443dc96ef232139fe2fcc0c079463b59627bd57
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-04-04 13:53:34 +0200

    Update data_processing.R

--- data_processing.R
+++ data_processing.R
@@ -19,17 +19,6 @@ prod <- zoo(data$values, order.by=dates)
 plot(prod)
 
 
-datafile_test <- "data_tp5.csv" 
-data_test <- read.csv(datafile_test,sep=";")
-
-dates_char <- as.character(data_test$dates)
-dates_char[1] #
-tail(dates_char,1) #
-dates <- as.yearmon(seq(from=1990, to=2023, by=1/12)) #
-spread <- zoo(data_test$spread, order.by=dates)
-dspread <- diff(spread,1)
-plot(cbind(spread,dspread))
-
 
 
 
